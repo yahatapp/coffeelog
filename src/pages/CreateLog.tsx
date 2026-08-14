1aec2f1c60f36dd20a9e1110a864c742a568a8da
Keny Yahat <65029527+ky12228121@users.noreply.github.com>
2026-08-14 23:35:41 +0900

    Make farm fields full width

--- src/pages/CreateLog.tsx
+++ src/pages/CreateLog.tsx
@@ -173,7 +173,7 @@ const CreateLogPage = () => {
               />
             </div>
 
-            <div>
+            <div className="col-span-2">
               <label className="text-xs font-bold text-cafe-text block mb-1.5">農園</label>
               <input
                 type="text"
